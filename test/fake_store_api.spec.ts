0c1f931509cc0ed1bf5a7fdd41321b15c6d5e6fd
Matheus Augusto Daminelli <matheus.90422@alunosatc.edu.br>
2025-09-23 20:20:32 -0300

    teste

--- test/fake_store_api.spec.ts
+++ test/fake_store_api.spec.ts
@@ -14,16 +14,14 @@ describe('API da Loja Falsa', () => {
 
   describe('PRODUTOS', () => {
     it('Obter todos os produtos', async () => {
+        await new Promise((resolve) => setTimeout(resolve, 2000)); // Atraso de 2 segundos
         await p
           .spec()
           .get(`${baseUrl}/products`)
           .withHeaders({
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
             'Referer': 'https://fakestoreapi.com/',
-            'Accept': 'application/json',
-            'Accept-Language': 'en-US,en;q=0.9',
-            'Connection': 'keep-alive',
-            'Cache-Control': 'no-cache'
+            'Accept': 'application/json'
           })
           .expectStatus(StatusCodes.OK)
           .expectJsonLength(20);
