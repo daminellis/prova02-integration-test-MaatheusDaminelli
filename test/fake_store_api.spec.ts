1f434a13789515cf11dba4f50e89fbfedb11b184
Matheus Augusto Daminelli <matheus.90422@alunosatc.edu.br>
2025-09-23 20:18:30 -0300

    teste

--- test/fake_store_api.spec.ts
+++ test/fake_store_api.spec.ts
@@ -20,7 +20,10 @@ describe('API da Loja Falsa', () => {
           .withHeaders({
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
             'Referer': 'https://fakestoreapi.com/',
-            'Accept': 'application/json'
+            'Accept': 'application/json',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Connection': 'keep-alive',
+            'Cache-Control': 'no-cache'
           })
           .expectStatus(StatusCodes.OK)
           .expectJsonLength(20);
