057b1de6cd4f40e7c6ad80fd9e888e49599688c4
Matheus Augusto Daminelli <matheus.90422@alunosatc.edu.br>
2025-09-23 20:05:36 -0300

    teste

---
+++ test/fake_store_api.spec.ts
@@ -0,0 +1,24 @@
+import pactum from 'pactum';
+import { StatusCodes } from 'http-status-codes';
+import { SimpleReporter } from '../simple-reporter';
+
+describe('API da Loja Falsa', () => {
+  const p = pactum;
+  const rep = SimpleReporter;
+  const baseUrl = 'https://fakestoreapi.com';
+
+  p.request.setDefaultTimeout(30000);
+
+  beforeAll(() => p.reporter.add(rep));
+  afterAll(() => p.reporter.end());
+
+  describe('PRODUTOS', () => {
+    it('Obter todos os produtos', async () => {
+      await p
+        .spec()
+        .get(`${baseUrl}/products`)
+        .expectStatus(StatusCodes.OK)
+        .expectJsonLength(20);
+    });
+  });
+});
