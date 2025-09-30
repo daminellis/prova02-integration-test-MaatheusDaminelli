d8eec907ddb657200842e189b46bcfb8166461f3
Matheus Augusto Daminelli <matheus.90422@alunosatc.edu.br>
2025-09-30 08:54:46 -0300

    teste

--- test/fake_store_api.spec.ts
+++ test/fake_store_api.spec.ts
@@ -1,62 +1,34 @@
-import pactum from 'pactum';
+import { spec } from 'pactum';
 import { StatusCodes } from 'http-status-codes';
-import { SimpleReporter } from '../simple-reporter';
 
-describe('API da Loja Falsa', () => {
-  const p = pactum;
-  const rep = SimpleReporter;
-  const baseUrl = 'https://fakestoreapi.com';
+const baseUrl = 'https://restful-booker.herokuapp.com';
 
-  p.request.setDefaultTimeout(30000);
-
-  beforeAll(() => p.reporter.add(rep));
-  afterAll(() => p.reporter.end());
-
-  describe('PRODUTOS', () => {
-    it('Obter todos os produtos', async () => {
-        const response = await p
-          .spec()
-          .get(`${baseUrl}/products`)
-          .toss();
-      });
+describe('Restful-booker API - Teste de Criação de Reserva', () => {
+  it('Deve criar uma nova reserva com sucesso', async () => {
+    await spec()
+      .post(`${baseUrl}/booking`)
+      .withHeaders({
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      })
+      .withJson({
+        firstname: 'Maria',
+        lastname: 'Silva',
+        totalprice: 150,
+        depositpaid: true,
+        bookingdates: {
+          checkin: '2025-09-24',
+          checkout: '2025-09-28'
+        },
+        additionalneeds: 'Breakfast'
+      })
+      .expectStatus(StatusCodes.OK) // Corrigido para aceitar o status 200
+      .expectJsonLike({
+        booking: {
+          firstname: 'Maria',
+          lastname: 'Silva'
+        }
+      })
+      .stores('bookingId', 'bookingid');
   });
-
-  describe('PRODUTOS', () => {
-    it('Obter produto específico pelo ID', async () => {
-        await p
-          .spec()
-          .get(`${baseUrl}/products/1`)
-          .withHeaders('User-Agent', 'pactum-test')
-          .expectStatus(StatusCodes.OK)
-          .expectJsonLike({
-            id: 1,
-            title: /.+/,
-            price: /.+/,
-            category: /.+/,
-        });
-    });
-  });
-
-  describe('PRODUTOS', () => {
-    it('Criar um novo produto', async () => {
-    await p
-        .spec()
-        .post(`${baseUrl}/products`)
-        .withHeaders('Content-Type', 'application/json')
-        .withJson({
-        title: 'Produto de Teste',
-        price: 99.99,
-        description: 'Descrição do produto de teste',
-        image: 'https://i.pravatar.cc',
-        category: 'electronics'
-        })
-        .expectStatus(StatusCodes.OK)
-        .expectJsonLike({
-        title: 'Produto de Teste',
-        price: 99.99,
-        category: 'electronics'
-        });
-    });
-  });
-
 });
