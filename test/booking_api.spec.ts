35de170d98f50bf80640c75d6306c5751d49c368
Matheus Augusto Daminelli <matheus.90422@alunosatc.edu.br>
2025-09-30 19:21:03 -0300

    'teste'

--- test/booking_api.spec.ts
+++ test/booking_api.spec.ts
@@ -3,6 +3,23 @@ import { StatusCodes } from 'http-status-codes';
 
 const baseUrl = 'https://restful-booker.herokuapp.com';
 
+describe('Restful-booker API - Autenticação', () => {
+  it('Deve criar um token de autenticação', async () => {
+    await spec()
+      .post(`${baseUrl}/auth`)
+      .withHeaders({
+        'Content-Type': 'application/json'
+      })
+      .withBody({
+        username: 'admin',
+        password: 'password123'
+      })
+      .expectStatus(StatusCodes.OK)
+      .expectJsonLike({ token: /\w+/ })
+      .stores('authToken', 'token');
+  });
+});
+
 describe('Restful-booker API - Buscar IDs de Reservas', () => {
   it('Deve retornar todos os IDs de reservas disponíveis', async () => {
     await spec()
@@ -34,5 +51,16 @@ describe('Restful-booker API - Buscar Detalhes da Reserva', () => {
           checkout: /\d{4}-\d{2}-\d{2}/
       }
     });
+  });
+});
+
+describe('Restful-booker API - Deletar Reserva', () => {
+  it('Deve deletar a reserva com sucesso', async () => {
+    await spec()
+      .delete(`${baseUrl}/booking/$S{firstBookingId}`)
+      .withHeaders({
+        'Authorization': 'Bearer $S{authToken}'
+      })
+      .expectStatus(StatusCodes.CREATED);
   });
 });
